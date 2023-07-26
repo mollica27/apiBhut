b00940cc5200c71eb55be2179b4f50ef25192d5c
Natan Mollica <natan.mollica@gmail.com>
2023-07-26 11:57:16 -0300

    configurando o arquivo gitignore

--- src/_server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-
-// Iniciar servidor
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-  console.log(`Acessar http://localhost:${port}`);
-});
